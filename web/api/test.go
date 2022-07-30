7ad09469da102909b6988e55f7b77266ce0e7038
akinoringo <wbraver@outlook.jp>
2022-07-30 14:46:18 +0900

    feat: サーバーから渡すサンプルデータを構造体の配列のJSONとして渡した

--- web/api/test.go
+++ web/api/test.go
@@ -1,13 +1,20 @@
 package api
 
 import (
+	"go-my-blog/models"
+
 	"github.com/labstack/echo/v4"
 	"github.com/valyala/fasthttp"
 )
 
 func Test() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		v := "test"
-		return c.JSON(fasthttp.StatusOK, v)
+		tests := []models.TestType{}
+		test1 := models.TestType{ID: "1", Name: "ジョナサン・ジョースター"}
+		test2 := models.TestType{ID: "2", Name: "ディオ・ブランドー"}
+		tests = append(tests, test1)
+		tests = append(tests, test2)
+
+		return c.JSON(fasthttp.StatusOK, tests)
 	}
 }
