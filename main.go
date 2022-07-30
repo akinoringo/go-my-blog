0c64d9c091b4cce2a0099b168d02f4a7cb11c27c
akinoringo <wbraver@outlook.jp>
2022-07-30 10:35:46 +0900

    feat: echoを用いたServerの起動

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+var e = createMux()
+
+func main() {
+	e.GET("/test", testHandler)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createMux() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	return e
+}
+
+func testHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!!")
+}
